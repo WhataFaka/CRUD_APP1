060f6cda7ffc0c2110be5e7fdfac76b252a61bf9
Your Name <mrmofd@gmail.com>
2023-02-01 23:33:19 +0700

    select and option + spring validator

--- src/main/java/ru/kityukha/springcourse/config/controllers/PeopleController.java
+++ src/main/java/ru/kityukha/springcourse/config/controllers/PeopleController.java
@@ -9,16 +9,19 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.kityukha.springcourse.config.dao.PersonDAO;
 import ru.kityukha.springcourse.config.models.Person;
+import ru.kityukha.springcourse.config.util.PersonValidator;
 
 @Controller
 @RequestMapping("/people")
 public class PeopleController {
 
     private final PersonDAO personDAO;
+    private final PersonValidator personValidator;
 
     @Autowired
-    public PeopleController(PersonDAO personDAO) {
+    public PeopleController(PersonDAO personDAO, PersonValidator personValidator) {
         this.personDAO = personDAO;
+        this.personValidator = personValidator;
     }
 
     @GetMapping()
@@ -41,6 +44,8 @@ public class PeopleController {
     @PostMapping()
     public String create(@ModelAttribute("person") @Valid Person person,
                          BindingResult bindingResult) {
+
+        personValidator.validate(person, bindingResult);
         if (bindingResult.hasErrors())
             return "people/new";
 
@@ -58,6 +63,7 @@ public class PeopleController {
     public String update(@ModelAttribute("person") Person person,
                          BindingResult bindingResult, @PathVariable("id") int id) {
 
+        personValidator.validate(person, bindingResult);
         if (bindingResult.hasErrors())
             return "people/edit";
 
